aa6dae1fc0fe3403ee10b92e935165d41b3c3a0c
vudkhoa <vudinhkhoaowl@gmail.com>
2025-09-29 23:29:07 +0700

    Update

--- Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -1,16 +1,16 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class JoystickPlayerExample : MonoBehaviour
-//{
-//    public float speed;
-//    public VariableJoystick variableJoystick;
-//    public Rigidbody rb;
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
 
-//    public void FixedUpdate()
-//    {
-//        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-//        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
-//    }
-//}
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+}
